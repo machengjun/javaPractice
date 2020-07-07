7dfcefeafbb0f14e83801f8c73ea0e9296a8d2b7
mcj <machengjun@larunda.com.cn>
2020-07-07 14:16:53 +0800

    属性拷贝

--- src/main/java/com/example/demo/rest/AuthorController.java
+++ src/main/java/com/example/demo/rest/AuthorController.java
@@ -2,17 +2,18 @@ package com.example.demo.rest;
 
 import com.alibaba.fastjson.JSON;
 import com.example.demo.dao.jpa.AuthorDao;
-import com.example.demo.dao.mapper.ArticleMapper;
-import com.example.demo.entity.Article;
 import com.example.demo.entity.Author;
-import com.example.demo.req.ArticleCreateReq;
 import com.example.demo.req.AuthorCreateDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 马成军
@@ -35,8 +36,7 @@ public class AuthorController {
         }
         //todo 属性拷贝
         Author author = new Author();
-        author.setName(dto.getName());
-        author.setAge(dto.getAge());
+        BeanUtils.copyProperties(dto, author);
         authorDao.save(author);
         return new ResponseEntity("添加成功", HttpStatus.OK);
     }
