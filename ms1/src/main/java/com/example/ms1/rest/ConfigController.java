747c10dfc6b3d4dcb22f81c6930681891fde4df8
mcj <machengjun@larunda.com.cn>
2020-07-28 15:00:33 +0800

    nacos注册

---
+++ ms1/src/main/java/com/example/ms1/rest/ConfigController.java
@@ -0,0 +1,29 @@
+package com.example.ms1.rest;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author 马成军
+ **/
+@RestController
+@RequestMapping("config")
+@RefreshScope
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+
+}
