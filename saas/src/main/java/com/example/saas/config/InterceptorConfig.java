12c281b0ed4d23d607f977a7a115297319996e42
mcj <machengjun@larunda.com.cn>
2020-07-30 08:56:11 +0800

    修改注释

--- saas/src/main/java/com/example/saas/config/InterceptorConfig.java
+++ saas/src/main/java/com/example/saas/config/InterceptorConfig.java
@@ -3,11 +3,12 @@ package com.example.saas.config;
 import com.example.saas.aop.RequestInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author 马成军
- * 也可以实现 WebMvcConfigurationSupport todo 新开专题研究spring boot
+ * 也可以继承 WebMvcConfigurationSupport todo 新开专题研究spring boot
  **/
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
